72c7cf6fc2b8d17df485e4b884ac45f16aff821f
MoriMorou <Mori_morou@rocketmail.com>
2019-02-20 12:49:16 +0300

    added new icons

--- SecretStorage/client/src/main/java/ru/morou/client/controllers/WorkFlowController.java
+++ SecretStorage/client/src/main/java/ru/morou/client/controllers/WorkFlowController.java
@@ -26,8 +26,8 @@ public class WorkFlowController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
-        LocalStorageView localStorageView = new LocalStorageView();
-        LocalStorage.getChildren().add(localStorageView.getView());
+//        LocalStorageView localStorageView = new LocalStorageView();
+//        LocalStorage.getChildren().add(localStorageView.getView());
     }
 
     @PostConstruct
